839f176ab54b7d499f1dcdede8c59beea241f47c
Larry Gadea <trivex@gmail.com>
2015-12-22 10:47:57 -0600

    Initial Commit

---
+++ HowOnline/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  HowOnline
+//
+//  Created by Larry Gadea on 12/22/15.
+//  Copyright © 2015 Larry Gadea. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
